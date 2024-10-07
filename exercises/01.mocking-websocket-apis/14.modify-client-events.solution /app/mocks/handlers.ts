f9e380ca5cdb466612a8c00c1ea83ea7ada4a605
Artem Zakharchenko <kettanaito@gmail.com>
2024-10-07 12:36:04 +0200

    update msw to rc-12

--- exercises/01.mocking-websocket-apis/14.modify-client-events.solution /app/mocks/handlers.ts
+++ exercises/01.mocking-websocket-apis/14.modify-client-events.solution /app/mocks/handlers.ts
@@ -9,41 +9,23 @@ const chat = ws.link('ws://127.0.0.1:56789/')
 export const handlers = [
   chat.addEventListener(
     'connection',
-    ({ server, client }) => {
-      // client.addEventListener(
-      //   'message',
-      //   (event) => {
-      //     client.send(event.data)
-      //   },
-      // )
-      server.connect()
+    ({ client }) => {
       client.addEventListener(
         'message',
         (event) => {
-          event.preventDefault()
-          const message = parseChatMessage(
-            event.data,
-          )
-          if (!message) {
-            return
-          }
-          message.data.author.name = 'MSW'
-          server.send(createChatMessage(message))
+          event.stopPropagation()
+          console.log(1)
         },
       )
-      server.addEventListener(
+    },
+  ),
+  chat.addEventListener(
+    'connection',
+    ({ client }) => {
+      client.addEventListener(
         'message',
         (event) => {
-          const message = parseChatMessage(
-            event.data,
-          )
-          if (!message) {
-            return
-          }
-          event.preventDefault()
-          message.data.text =
-            message.data.text.toUpperCase()
-          client.send(createChatMessage(message))
+          console.log(2)
         },
       )
     },
